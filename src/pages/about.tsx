4052795de0dc252bfded8288d5c5da84089e5a9d
wraithwinterly <softwinner360@gmail.com>
2023-01-16 13:48:51 -0500

    update text

---
+++ src/pages/about.tsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Specialized in web and game development. Over 2 years of experience as a
+        game developer, with engines such as Unreal Engine, Godot and Unity,
+        having released his solo developed first game "Everplast" on Steam
+        (https://store.steampowered.com/app/1896630/Everplast/) which counts as
+        his biggest achievement to the date. His first experience in web3 was
+        with Pigeon DAO (https://app.pigeondao.org/), where he worked as an
+        advisor and web3 developer. Currently has moved to work on Mothora to
+        build the game systems and game web app user experience. Portfolio:
+        https://wraithwinterly.github.io; Github:
+        https://github.com/WraithWinterly
+      </p>
+    </div>
+  );
+}
