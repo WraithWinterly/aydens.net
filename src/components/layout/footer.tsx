4d4cbd7e54679e2eda039ec31840fd62780c40f1
Ayden Springer <37941646+WraithWinterly@users.noreply.github.com>
2023-04-25 21:41:37 -0400

    improve footer bg consitency

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -21,38 +21,41 @@ const Socials = [
 export default function Footer() {
   const id = useId();
   return (
-    <footer className="relative flex w-full justify-center bg-bgPurple bg-opacity-70 py-7 text-gray-300 dark:border-slate-500">
-      <div className="flex flex-col items-center gap-2">
-        <p className="mx-auto flex flex-wrap items-center justify-center gap-2 text-center">
-          <span>Designed and programmed with</span>
-          <span className="text-2xl text-red-600"> &hearts; </span>
-          {/* <FiHeart className="text-red-500" /> */}
-          <span>by Ayden Springer.</span>
-        </p>
-        <div className="my-2 flex items-center gap-4">
-          {Socials.map((social, i) => (
-            <a href={social.link} target="_blank" rel="noreferrer noopener">
-              <social.icon
-                size={36}
-                className="text-gray-300 transition-colors duration-500 hover:text-accent"
-                key={`${id}-${i}`}
-              />
-            </a>
-          ))}
-        </div>
+    <footer className="relative w-full bg-black text-gray-300 dark:border-slate-500">
+      <div className="flex w-full justify-center bg-bgPurple bg-opacity-70 p-7">
+        {" "}
+        <div className="flex flex-col items-center gap-2">
+          <p className="mx-auto flex flex-wrap items-center justify-center gap-2 text-center">
+            <span>Designed and programmed with</span>
+            <span className="text-2xl text-red-600"> &hearts; </span>
+            {/* <FiHeart className="text-red-500" /> */}
+            <span>by Ayden Springer.</span>
+          </p>
+          <div className="my-2 flex items-center gap-4">
+            {Socials.map((social, i) => (
+              <a href={social.link} target="_blank" rel="noreferrer noopener">
+                <social.icon
+                  size={36}
+                  className="text-gray-300 transition-colors duration-500 hover:text-accent"
+                  key={`${id}-${i}`}
+                />
+              </a>
+            ))}
+          </div>
 
-        <div className="flex flex-col items-center gap-4 md:flex-row">
-          <a href={`https://${process.env.NEXT_PUBLIC_URL}`}>
-            <span className="text-xl">{process.env.NEXT_PUBLIC_URL}</span>
-          </a>
-          <p className="hidden md:inline-block">&bull;</p>
-          <p>&copy;2022-{new Date().getFullYear()}</p>
-          <p className="hidden md:inline-block">&bull;</p>
-          <a
-            href="https://github.com/WraithWinterly/aydens.net"
-            className="text-lg hover:text-accent">
-            View Source
-          </a>
+          <div className="flex flex-col items-center gap-4 md:flex-row">
+            <a href={`https://${process.env.NEXT_PUBLIC_URL}`}>
+              <span className="text-xl">{process.env.NEXT_PUBLIC_URL}</span>
+            </a>
+            <p className="hidden md:inline-block">&bull;</p>
+            <p>&copy;2022-{new Date().getFullYear()}</p>
+            <p className="hidden md:inline-block">&bull;</p>
+            <a
+              href="https://github.com/WraithWinterly/aydens.net"
+              className="text-lg hover:text-accent">
+              View Source
+            </a>
+          </div>
         </div>
       </div>
     </footer>
