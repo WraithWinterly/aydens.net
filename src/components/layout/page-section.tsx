014aaeca85c91e4e2e8a4ca6df1cda226398d153
Ayden Springer <37941646+WraithWinterly@users.noreply.github.com>
2023-05-12 19:31:53 -0400

    Enhancements and bug fixes

--- src/components/layout/page-section.tsx
+++ src/components/layout/page-section.tsx
@@ -2,7 +2,11 @@ import { ReactNode } from "react";
 import space1 from "@assets/space1.jpg";
 import space2 from "@assets/space2.jpg";
 import ellipseBlue from "@assets/ellipse-blue.png";
+import ellipseBlue2 from "@assets/ellipse-blue2.png";
+
 import ellipsePurple from "@assets/ellipse-purple.png";
+import ellipsePurple2 from "@assets/ellipse-purple2.png";
+
 import Image from "next/image";
 
 export default function PageSection({
@@ -21,7 +25,7 @@ export default function PageSection({
         backgroundImage: `url(${color === "blue" ? space2.src : space1.src})`,
       }}>
       <div
-        className={`absolute ${
+        className={`pointer-events-none absolute select-none ${
           color === "blue" ? "right-0 bottom-0" : "left-0 bottom-0"
         }`}>
         <Image
@@ -32,8 +36,19 @@ export default function PageSection({
         />
       </div>
       <div
+        className={`pointer-events-none absolute select-none ${
+          color === "blue" ? "-top-80 -left-20" : "right-0 bottom-0"
+        }`}>
+        <Image
+          src={color === "blue" ? ellipseBlue2.src : ellipsePurple2.src}
+          alt="ellipse"
+          width={800}
+          height={800}
+        />
+      </div>
+      <div
         className={`min-h-[calc(100vh+100px)] w-full ${
-          color === "blue" ? "bg-bgBlue/[87%]" : "bg-bgPurple/80"
+          color === "blue" ? "bg-bgBlue/90" : "bg-bgPurple/90"
         } `}>
         <div
           className={`flex min-h-[calc(100vh+100px)] flex-col py-16 px-6 md:px-8 ${
