2e1446a529fe0f74df82abb45290beec93ff451a
Ayden Springer <37941646+WraithWinterly@users.noreply.github.com>
2023-05-12 19:50:47 -0400

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import Layout from "@/components/layout/layout";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-46XMST9KKY"
+      />
+      <Script>{`function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-46XMST9KKY");`}</Script>
     </Layout>
   );
 }
